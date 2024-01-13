94e05cbda6487e6c292b3f240fbeb0653bb99c42
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-13 19:06:33 +0700

    chore(page): update dashboard links page
    - implement public metadata checking

--- src/app/(authenticated)/dashboard/links/page.tsx
+++ src/app/(authenticated)/dashboard/links/page.tsx
@@ -1,11 +1,19 @@
 import { type Metadata } from "next";
 import { type FC } from "react";
+import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Dashboard Links | Ikram Maulana Links",
 };
 
-const page: FC = () => {
+const page: FC = async () => {
+  const publicMetadata = await api.publicMetadata.available.query();
+
+  if (!publicMetadata || "error" in publicMetadata) {
+    return redirect("/dashboard/settings");
+  }
+
   return (
     <div className="mb-6 flex flex-col">
       <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
