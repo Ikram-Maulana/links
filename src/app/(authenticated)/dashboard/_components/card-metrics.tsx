f0f9bca03d95b4591864706217d5b6932adf72ac
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-13 19:02:38 +0700

    chore(component): update card metrics component
    - implement public metadata checking

--- src/app/(authenticated)/dashboard/_components/card-metrics.tsx
+++ src/app/(authenticated)/dashboard/_components/card-metrics.tsx
@@ -1,7 +1,8 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { api } from "@/trpc/server";
-import { IconCircleX, IconLink } from "@irsyadadl/paranoid";
+import { IconCircleX, IconLink, IconTriangleInfo } from "@irsyadadl/paranoid";
+import Link from "next/link";
 import { type FC } from "react";
 
 interface metricsProps {
@@ -11,7 +12,10 @@ interface metricsProps {
 }
 
 const CardMetrics: FC = async () => {
-  const metrics = (await api.metrics.getAll.query()) as unknown as metricsProps;
+  const [metrics, publicMetadata] = await Promise.all([
+    api.metrics.getAll.query() as unknown as metricsProps,
+    api.publicMetadata.available.query(),
+  ]);
 
   if (!metrics || "error" in metrics) {
     return (
@@ -37,21 +41,40 @@ const CardMetrics: FC = async () => {
   ];
 
   return (
-    <div className="grid grid-flow-row grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      {metricsData.map((metric, index) => (
-        <Card key={`metric-${index}`}>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              {metric.title}
-            </CardTitle>
-            {metric.icon}
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{metric.value}</div>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+    <>
+      {(!publicMetadata || "error" in publicMetadata) && (
+        <Alert
+          className="mb-4 border-amber-500 bg-amber-500 text-zinc-50 dark:border-amber-900 dark:bg-amber-900 dark:text-zinc-50"
+          variant="destructive"
+        >
+          <IconTriangleInfo className="h-4 w-4 !text-[#fafafa]" />
+          <AlertTitle>Warning</AlertTitle>
+          <AlertDescription>
+            You haven&apos;t filled in the basic information, please fill it in
+            first{" "}
+            <Link href="/dashboard/settings" className="underline">
+              here
+            </Link>
+            .
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="grid grid-flow-row grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        {metricsData.map((metric, index) => (
+          <Card key={`metric-${index}`}>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                {metric.title}
+              </CardTitle>
+              {metric.icon}
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{metric.value}</div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 };
 
