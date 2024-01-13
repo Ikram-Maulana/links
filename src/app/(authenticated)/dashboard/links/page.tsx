0a9e14bbfc72b609e7156dc70412712f673ca7fc
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-13 20:33:46 +0700

    chore(component): implement data table links and make skeleton

--- src/app/(authenticated)/dashboard/links/page.tsx
+++ src/app/(authenticated)/dashboard/links/page.tsx
@@ -1,7 +1,9 @@
-import { type Metadata } from "next";
-import { type FC } from "react";
 import { api } from "@/trpc/server";
+import { type Metadata } from "next";
 import { redirect } from "next/navigation";
+import { Suspense, type FC } from "react";
+import Content from "./_components/content";
+import DataTableSkeleton from "./_components/skeleton/data-table-skeleton";
 
 export const metadata: Metadata = {
   title: "Dashboard Links | Ikram Maulana Links",
@@ -15,14 +17,20 @@ const page: FC = async () => {
   }
 
   return (
-    <div className="mb-6 flex flex-col">
-      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-        Links
-      </h1>
-      <p className="text-sm font-medium leading-6 text-gray-500">
-        This page is used to manage links.
-      </p>
-    </div>
+    <>
+      <div className="mb-6 flex flex-col">
+        <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Links
+        </h1>
+        <p className="text-sm font-medium leading-6 text-gray-500">
+          This page is used to manage links.
+        </p>
+      </div>
+
+      <Suspense fallback={<DataTableSkeleton />}>
+        <Content />
+      </Suspense>
+    </>
   );
 };
 
