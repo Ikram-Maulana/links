6acd6b37d63a5833a088b1ca463404b906bd9e27
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-16 12:18:53 +0700

    chore(utils): add get base url method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { env } from "@/env";
 import { clsx, type ClassValue } from "clsx";
 import { cloneElement, type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
@@ -10,4 +11,19 @@ export const applyIconClassNames = (icon: ReactNode, classNames = "") => {
   return cloneElement(icon as React.ReactElement, {
     className: `${classNames}`,
   });
+};
+
+export const getBaseUrl = () => {
+  const IS_SERVER = typeof window === "undefined";
+  let urlArr: string[] = [];
+
+  if (!IS_SERVER) {
+    const url = window.location.href;
+    urlArr = url.split("/");
+  }
+
+  const baseUrl = IS_SERVER
+    ? env.NEXT_PUBLIC_BASE_URL
+    : `${urlArr[0]}//${urlArr[2]}`;
+  return baseUrl;
 };
