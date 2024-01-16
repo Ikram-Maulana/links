b66d52b07344e2a08b9d4ebe5331379d50d96fa4
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-16 20:37:18 +0700

    chore(api): add detail and update links list method tRPC API

--- src/server/api/routers/links-list.ts
+++ src/server/api/routers/links-list.ts
@@ -76,6 +76,97 @@ export const linksListRouter = createTRPCRouter({
       }
     }),
 
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        id: z
+          .string({
+            required_error: "ID is required",
+          })
+          .min(3, {
+            message: "Please provide a valid ID",
+          }),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db
+          .select()
+          .from(linksList)
+          .where(eq(linksList.id, input.id));
+
+        return data;
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(error.message);
+        }
+
+        return {
+          error: "Server Error",
+        };
+      }
+    }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string({
+          required_error: "ID is required",
+        }),
+        image: z.string({
+          required_error: "Image is required",
+        }),
+        title: z
+          .string({
+            required_error: "Title is required",
+          })
+          .trim()
+          .min(3, "Please enter a title"),
+        url: z
+          .string({
+            required_error: "URL is required",
+          })
+          .url({
+            message: "URL is invalid",
+          }),
+        slug: z
+          .string({
+            required_error: "Slug is required",
+          })
+          .trim()
+          .min(3, {
+            message: "Slug must be at least 3 characters",
+          }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const inputImage = input.image !== "" ? { image: input.image } : {};
+
+        const data = await ctx.db
+          .update(linksList)
+          .set({
+            ...inputImage,
+            title: input.title,
+            url: input.url,
+            slug: input.slug,
+            updatedAt: new Date(),
+          })
+          .where(eq(linksList.id, input.id))
+          .returning();
+
+        return data;
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(error.message);
+        }
+
+        return {
+          error: "Server Error",
+        };
+      }
+    }),
+
   delete: protectedProcedure
     .input(
       z.object({
