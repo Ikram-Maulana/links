6a75e38b4e3923d07a4e93b091e92a010f6d8c55
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-16 12:18:38 +0700

    chore(api): add create links list method tRPC API

--- src/server/api/routers/links-list.ts
+++ src/server/api/routers/links-list.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { linksList } from "@/server/db/schema";
 import { desc } from "drizzle-orm";
+import { z } from "zod";
 
 export const linksListRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
@@ -21,4 +22,53 @@ export const linksListRouter = createTRPCRouter({
       };
     }
   }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        image: z.string({
+          required_error: "Image is required",
+        }),
+        title: z.string({
+          required_error: "Title is required",
+        }),
+        url: z
+          .string({
+            required_error: "URL is required",
+          })
+          .url({
+            message: "URL is invalid",
+          }),
+        slug: z
+          .string({
+            required_error: "Slug is required",
+          })
+          .min(3, {
+            message: "Slug must be at least 3 characters",
+          }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const data = await ctx.db
+          .insert(linksList)
+          .values({
+            image: input.image,
+            title: input.title,
+            url: input.url,
+            slug: input.slug,
+          })
+          .returning();
+
+        return data;
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(error.message);
+        }
+
+        return {
+          error: "Server Error",
+        };
+      }
+    }),
 });
