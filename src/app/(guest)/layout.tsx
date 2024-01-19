21516623c0671c94db5266c8b29c33f217e00b8c
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-19 19:31:21 +0700

    chore(component): add topbar component

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,5 +1,12 @@
+import dynamic from "next/dynamic";
 import { type FC } from "react";
 import Footer from "./_components/footer";
+import TopbarSkeleton from "./_components/skeleton/topbar-skeleton";
+
+const Topbar = dynamic(() => import("./_components/topbar"), {
+  loading: () => <TopbarSkeleton />,
+  ssr: false,
+});
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,7 +15,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col">
-      {/* Navbar Here */}
+      <Topbar />
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
