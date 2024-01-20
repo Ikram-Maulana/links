f28bcd72e24df3d86fc5db7dd78dd6ddd9688315
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-20 11:47:16 +0700

    chore(page): implement links list on homepage

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from "react";
+import LinksList from "./_components/links-list";
+import LinksListSkeleton from "./_components/links-list/skeleton/links-list-skeleton";
 import Profile from "./_components/profile";
 import ProfileSkeleton from "./_components/skeleton/profile-skeleton";
 
@@ -8,6 +10,10 @@ export default async function Home() {
       <div className="mx-auto max-w-[680px]">
         <Suspense fallback={<ProfileSkeleton />}>
           <Profile />
+        </Suspense>
+
+        <Suspense fallback={<LinksListSkeleton nCard={5} />}>
+          <LinksList />
         </Suspense>
       </div>
     </main>
