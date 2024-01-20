7ad0a0d958a36a6aeb37062c6fde0a2424b3d7be
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-20 11:46:58 +0700

    chore(component): add links list component
    - add card component with framer animation
    - add links list share button on card
    - add links list skeleton

---
+++ src/app/(guest)/_components/links-list/card.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { type FC } from "react";
+
+const ShareButton = dynamic(() => import("./share-button"), {
+  loading: () => (
+    <Skeleton className="absolute bottom-0 right-[6px] top-0 z-[1] mx-0 my-auto h-10 w-10 rounded-full" />
+  ),
+  ssr: false,
+});
+
+interface CardProps {
+  image: string | null;
+  title: string;
+  url: string;
+  slug: string;
+}
+
+const Card: FC<CardProps> = ({ image = "", title, url, slug }) => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      className="hover:custor-pointer group relative mb-4 h-auto w-full overflow-hidden rounded-lg bg-white shadow-sm"
+    >
+      <a
+        href={`${url}?utm_source=ikramlinks&utm_medium=redirect&utm_campaign=ikramlinks`}
+        className={cn(
+          "text-break relative flex h-auto min-h-14 w-full items-center justify-center px-[66px] py-4 text-center",
+          {
+            "px-[44px]": image === "" && !image,
+          },
+        )}
+        target="_blank"
+        rel="noopener"
+      >
+        <div className="h-full w-full">
+          <div className="absolute left-1 top-1/2 flex h-12 w-12 -translate-y-1/2 items-center	justify-center overflow-hidden rounded-md">
+            {image !== "" && image && (
+              <Image
+                src={image}
+                alt={title}
+                layout="fill"
+                sizes="100vw"
+                className="h-full w-full object-cover"
+              />
+            )}
+          </div>
+
+          <p className="h-fit font-medium leading-6">{title}</p>
+        </div>
+      </a>
+
+      <ShareButton slug={slug} />
+    </motion.div>
+  );
+};
+
+export default Card;
---
+++ src/app/(guest)/_components/links-list/index.tsx
@@ -0,0 +1,66 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Skeleton } from "@/components/ui/skeleton";
+import { type linksList } from "@/server/db/schema";
+import { api } from "@/trpc/server";
+import { IconCircleX, IconTriangleInfo } from "@irsyadadl/paranoid";
+import { type InferSelectModel } from "drizzle-orm";
+import { unstable_noStore as noStore } from "next/cache";
+import dynamic from "next/dynamic";
+import { type FC } from "react";
+
+const Card = dynamic(() => import("./card"), {
+  loading: () => <Skeleton className="mb-4 h-14 w-full" />,
+  ssr: false,
+});
+
+type LinksDataProps = InferSelectModel<typeof linksList>;
+
+const LinksList: FC = async () => {
+  noStore();
+  const links =
+    (await api.linksList.getAll.query()) as unknown as LinksDataProps[];
+
+  if (!links) {
+    return (
+      <Alert
+        className="mt-8 border-amber-500 bg-amber-500 text-zinc-50 dark:border-amber-900 dark:bg-amber-900 dark:text-zinc-50"
+        variant="destructive"
+      >
+        <IconTriangleInfo className="h-4 w-4 !text-[#fafafa]" />
+        <AlertTitle>Warning</AlertTitle>
+        <AlertDescription>Sorry, the link list is empty.</AlertDescription>
+      </Alert>
+    );
+  }
+
+  if ("error" in links) {
+    return (
+      <Alert
+        className="mt-8 border-red-500 bg-red-500 text-zinc-50 dark:border-red-900 dark:bg-red-900 dark:text-zinc-50"
+        variant="destructive"
+      >
+        <IconCircleX className="h-4 w-4 !text-[#fafafa]" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          An error occurred while loading data.
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="mt-8">
+      {links.map((link) => (
+        <Card
+          key={`linkslist-${link.id}`}
+          title={link.title}
+          url={link.url}
+          slug={link.slug}
+          image={link.image}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default LinksList;
