3b8f2b7ad8fbdae558a30bd52923634b97604c17
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-26 21:58:03 +0700

    refactor(image): update image blur to give more control

--- src/components/images/dynamic/blur.tsx
+++ src/components/images/dynamic/blur.tsx
@@ -1,31 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
-import { type FC } from "react";
+import React from "react";
 
-interface DynamicImagesBlurProps {
+export interface DynamicImageBlurProps
+  extends React.ComponentPropsWithoutRef<typeof Image> {
   src: string;
   alt: string;
 }
 
-const DynamicImagesBlur: FC<DynamicImagesBlurProps> = async ({ src, alt }) => {
-  const buffer = await fetch(src).then(async (res) => {
-    return Buffer.from(await res.arrayBuffer());
-  });
+const DynamicImagesBlur = React.forwardRef<
+  HTMLImageElement,
+  DynamicImageBlurProps
+>(async ({ src, alt, className, ...props }, ref) => {
+  const response = await fetch(src);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
   const { base64 } = await getPlaiceholder(buffer);
 
   return (
     <Image
       src={src}
       alt={alt}
-      className="h-full w-full object-cover"
+      className={cn("h-full w-full object-cover", className)}
       fill
       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       placeholder="blur"
       blurDataURL={base64}
+      {...props}
+      ref={ref}
     />
   );
-};
+});
 
+DynamicImagesBlur.displayName = "DynamicImagesBlur";
 export default DynamicImagesBlur;
