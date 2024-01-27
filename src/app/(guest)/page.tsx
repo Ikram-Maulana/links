38bf6ebad94717284dfceb54355e89eb2c3fa1e4
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-27 13:34:06 +0700

    style(homepage): merge container with max width

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -6,16 +6,14 @@ import ProfileSkeleton from "./_components/skeleton/profile-skeleton";
 
 export default async function Home() {
   return (
-    <main className="container px-4">
-      <div className="mx-auto w-screen max-w-[680px]">
-        <Suspense fallback={<ProfileSkeleton />}>
-          <Profile />
-        </Suspense>
+    <main className="container mx-auto w-screen max-w-[680px] px-4 md:px-0">
+      <Suspense fallback={<ProfileSkeleton />}>
+        <Profile />
+      </Suspense>
 
-        <Suspense fallback={<LinksListSkeleton nCard={5} />}>
-          <LinksList />
-        </Suspense>
-      </div>
+      <Suspense fallback={<LinksListSkeleton nCard={5} />}>
+        <LinksList />
+      </Suspense>
     </main>
   );
 }
