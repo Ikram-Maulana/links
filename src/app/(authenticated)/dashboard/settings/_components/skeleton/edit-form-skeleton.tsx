b196dce7cb3d02bc1347e565e051c2aef6f68088
Ikram Maulana <ikram075@ummi.ac.id>
2024-02-21 15:32:14 +0700

    chore(skeleton): update edit settings form skeleton

--- src/app/(authenticated)/dashboard/settings/_components/skeleton/edit-form-skeleton.tsx
+++ src/app/(authenticated)/dashboard/settings/_components/skeleton/edit-form-skeleton.tsx
@@ -5,15 +5,17 @@ const EditFormSkeleton: FC = () => {
   return (
     <div className="space-y-4">
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[95px]" />
+        <div>
+          <Skeleton className="h-4 w-[95px]" />
+          <Skeleton className="mb-4 mt-1 h-4 w-[105px]" />
+        </div>
 
         <div className="flex flex-col gap-4 md:flex-row md:items-center">
-          <Skeleton className="h-24 w-24 flex-shrink-0 rounded-full" />
+          <Skeleton className="mx-auto h-24 w-24 flex-shrink-0 rounded-full" />
 
           <div className="flex w-full flex-col gap-y-2">
-            <Skeleton className="h-9 w-full" />
-            <Skeleton className="h-2 w-full" />
-            <Skeleton className="h-[19px] w-full" />
+            <Skeleton className="mt-[10px] h-[60px] w-full" />
+            <Skeleton className="mx-auto mb-[10px] h-[17px] w-[127px]" />
           </div>
         </div>
       </div>
