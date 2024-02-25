aac4998bab5dade54d5d659f4fa0f616862f9cdf
Ikram Maulana <ikram075@ummi.ac.id>
2024-02-25 15:21:27 +0700

    chore(component): update profile component
    - change from dynamic image to default nextjs image component, dynamic image cause error "hydration"

--- src/app/(guest)/_components/profile.tsx
+++ src/app/(guest)/_components/profile.tsx
@@ -1,4 +1,3 @@
-import { DynamicImagesBlur } from "@/components/images/dynamic/blur";
 import { env } from "@/env";
 import { isValidUrl } from "@/lib/utils";
 import { type publicMetadata, type users } from "@/server/db/schema";
@@ -6,6 +5,7 @@ import { api } from "@/trpc/server";
 import { IconLocation } from "@irsyadadl/paranoid";
 import { type InferSelectModel } from "drizzle-orm";
 import { unstable_noStore as noStore } from "next/cache";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import { type FC } from "react";
 
@@ -40,7 +40,14 @@ export const Profile: FC = async () => {
   return (
     <div className="flex flex-col items-center pt-16">
       <div className="relative mb-4 h-24 w-24 overflow-hidden rounded-full">
-        <DynamicImagesBlur src={imageUrl} alt={profile.name ?? ""} />
+        <Image
+          src={imageUrl}
+          alt={profile.name ?? ""}
+          className="h-full w-full object-cover"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority
+        />
       </div>
 
       <h1 className="w-fit scroll-m-20 text-xl font-semibold tracking-tight">
