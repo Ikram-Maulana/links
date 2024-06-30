ff43309dc758e9e4d54a7f005d9e77e51280d748
Ikram Maulana <ikram075@ummi.ac.id>
2024-06-30 16:11:50 +0700

    chore(api): add get all overview tRPC API

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { overviewRouter } from "@/server/api/routers/overview";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -5,7 +6,9 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  overview: overviewRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
