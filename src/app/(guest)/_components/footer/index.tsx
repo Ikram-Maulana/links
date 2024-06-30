0ee25219a43ba40670a101462763cc12bed076b1
Ikram Maulana <ikram075@ummi.ac.id>
2024-06-30 13:29:30 +0700

    chore(component): initialize footer components

---
+++ src/app/(guest)/_components/footer/index.tsx
@@ -0,0 +1,13 @@
+import { type FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <footer className="container py-10">
+      <div className="mx-auto max-w-[680px] text-center">
+        <p className="text-sm font-medium leading-5">
+          Made with ❤️ by Ikram Maulana
+        </p>
+      </div>
+    </footer>
+  );
+};
--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,4 +1,5 @@
 import { type FC } from "react";
+import { Footer } from "./_components/footer";
 import { TopGradient } from "./_components/top-gradient";
 import Topbar from "./_components/topbar";
 
@@ -16,7 +17,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
       <div className="flex h-full w-full flex-col items-center">
         <Topbar />
         <main className="relative w-full flex-grow">{children}</main>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );
