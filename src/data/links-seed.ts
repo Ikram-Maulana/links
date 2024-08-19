5ee1a6992d7050c80d747fb4ab552357697d362e
Ikram-Maulana <ikram075@ummi.ac.id>
2024-08-19 07:53:44 +0700

    feat(db): add links seed data
    - add links seed data to populate the database with initial links.

---
+++ src/data/links-seed.ts
@@ -0,0 +1,27 @@
+export const linksSeedData = [
+  {
+    title: "Web Portfolio",
+    url: "https://ikrammaulana.my.id",
+    slug: "web-portfolio",
+  },
+  {
+    title: "Github",
+    url: "https://github.com/Ikram-Maulana",
+    slug: "github",
+  },
+  {
+    title: "Linkedin",
+    url: "https://www.linkedin.com/in/ikram-maulana",
+    slug: "linkedin",
+  },
+  {
+    title: "Instagram",
+    url: "https://instagram.com/ikram_maulana",
+    slug: "instagram",
+  },
+  {
+    title: "Dribbble",
+    url: "https://dribbble.com/ikram_maulana",
+    slug: "dribbble",
+  },
+];
---
+++ src/server/db/seed.ts
@@ -0,0 +1,50 @@
+import { linksSeedData } from "@/data";
+import { eq, sql } from "drizzle-orm";
+import { db } from ".";
+import { links } from "./schema";
+
+const seedLinks = async () => {
+  console.log("Seeding links...");
+
+  const startTime = performance.now();
+
+  try {
+    await db.transaction(async () => {
+      for (const link of linksSeedData) {
+        const linkExistsPrepared = db
+          .select()
+          .from(links)
+          .where(eq(links.slug, sql.placeholder("slug")))
+          .prepare("checkLinkExists");
+        const [existingLink] = await linkExistsPrepared.execute({
+          slug: link.slug,
+        });
+
+        if (existingLink) {
+          console.error(`Link with slug ${link.slug} already exists.`);
+          continue;
+        }
+
+        const insertLinkPrepared = db
+          .insert(links)
+          .values(link)
+          .returning()
+          .prepare("insertLink");
+        await insertLinkPrepared.execute();
+        console.log(`Link with slug ${link.slug} has been inserted.`);
+      }
+    });
+
+    console.log("Seeding links done.");
+  } catch (error) {
+    console.error("Error seeding links:", error);
+  } finally {
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+    console.log(`Execution time: ${executionTime.toFixed(2)}ms`);
+
+    process.exit();
+  }
+};
+
+await seedLinks();
