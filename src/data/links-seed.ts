bbc5efd1e3a6f937b202e38f99fa7b7e8df9f0dd
Ikram-Maulana <ikram075@ummi.ac.id>
2024-08-19 16:49:20 +0700

    chore(db): add isPublished field to links seed data
    - add the isPublished field to the links seed data in order to indicate whether a link should be published or not.

--- src/data/links-seed.ts
+++ src/data/links-seed.ts
@@ -3,25 +3,30 @@ export const linksSeedData = [
     title: "Web Portfolio",
     url: "https://ikrammaulana.my.id",
     slug: "web-portfolio",
+    isPublished: true,
   },
   {
     title: "Github",
     url: "https://github.com/Ikram-Maulana",
     slug: "github",
+    isPublished: true,
   },
   {
     title: "Linkedin",
     url: "https://www.linkedin.com/in/ikram-maulana",
     slug: "linkedin",
+    isPublished: true,
   },
   {
     title: "Instagram",
     url: "https://instagram.com/ikram_maulana",
     slug: "instagram",
+    isPublished: true,
   },
   {
     title: "Dribbble",
     url: "https://dribbble.com/ikram_maulana",
     slug: "dribbble",
+    isPublished: true,
   },
 ];
