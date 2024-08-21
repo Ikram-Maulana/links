3236c4fcc55458cc6cee4a40ebfd197d565ad052
Ikram-Maulana <ikram075@ummi.ac.id>
2024-08-21 17:46:20 +0700

    chore(config): update tailwind.config.ts
    - update tailwindCssAnimate import strategy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindCssAnimate from "tailwindcss-animate";
 
 const config = {
   darkMode: ["class"],
@@ -83,7 +84,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindCssAnimate],
 } satisfies Config;
 
 export default config;
